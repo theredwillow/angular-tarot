c46bee92edfa5d018945a4f4bdede8f01cec6b0c
theredwillow <theredwillows@gmail.com>
2022-07-15 21:31:38 -0500

    Fixing step/step folder mistake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StepComponent } from './step/step/step.component';
+import { StepComponent } from './step/step.component';
 
 @NgModule({
   declarations: [
