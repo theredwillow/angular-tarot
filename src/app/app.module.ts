41774652b9af36bdc4d3d99b3f0650ff07e20f7f
theredwillow <theredwillows@gmail.com>
2022-07-21 17:34:34 -0500

    ng generate component title-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleInputComponent } from './title-input/title-input.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TitleInputComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/title-input/title-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-input',
+  templateUrl: './title-input.component.html',
+  styleUrls: ['./title-input.component.scss']
+})
+export class TitleInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
