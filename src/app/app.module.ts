538ff23dd533398324b55f29fbc961e60c6f7221
theredwillow <theredwillows@gmail.com>
2022-07-21 17:36:05 -0500

    ng generate component positions-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleInputComponent } from './title-input/title-input.component';
+import { PositionsInputComponent } from './positions-input/positions-input.component';
 
 @NgModule({
-  declarations: [AppComponent, TitleInputComponent],
+  declarations: [AppComponent, TitleInputComponent, PositionsInputComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/positions-input/positions-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-positions-input',
+  templateUrl: './positions-input.component.html',
+  styleUrls: ['./positions-input.component.scss']
+})
+export class PositionsInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
